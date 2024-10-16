0e1d459879cd1f3c630e4a92994f1d1ccc48dbc7
hunor64 <nhunor06@gmail.com>
2024-10-16 10:41:40 +0200

    Added new comment

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -48,6 +48,7 @@ namespace PDFILE
         }
         public bool IsHeader(string line)
         {
+            //line.Contains(" - ")
             if (line.)
             {
                 return true;
