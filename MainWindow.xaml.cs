308b7a5477e64043b61082d3409df1a3e6882bb8
Placko135 <papplaszlolevente@gmail.com>
2024-10-18 10:50:40 +0200

    Lyonatán the best

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,62 +15,108 @@ using iText.Kernel.Pdf.Canvas.Parser;
 namespace PDFILE
 {
     #region generated tcp
-   /* using System;
+ /*   // A C# program for Client
+    using System;
     using System.Net;
     using System.Net.Sockets;
     using System.Text;
 
-    class TcpServerExample
+    namespace Client
     {
-        static void Main(string[] args)
+
+        class Program
         {
-            // Define the server's IP address and port
-            string serverIp = "127.0.0.1"; // Change this to the server's IP address if needed
-            int port = 8080; // Same port as the client
 
-            // Create a TCP listener
-            TcpListener server = new TcpListener(IPAddress.Parse(serverIp), port);
-            server.Start();
-            Console.WriteLine("Server started, waiting for a connection...");
-
-            try
+            // Main Method
+            static void Main(string[] args)
             {
-                // Accept incoming client connection
-                using (TcpClient client = server.AcceptTcpClient())
+                ExecuteClient();
+            }
+
+            // ExecuteClient() Method
+            static void ExecuteClient()
+            {
+
+                try
                 {
-                    Console.WriteLine("Client connected.");
 
-                    // Get the network stream
-                    NetworkStream stream = client.GetStream();
+                    // Establish the remote endpoint 
+                    // for the socket. This example 
+                    // uses port 11111 on the local 
+                    // computer.
+                    IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+                    IPAddress ipAddr = ipHost.AddressList[0];
+                    IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
 
-                    // Buffer for reading data
-                    byte[] buffer = new byte[256];
-                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
-                    string receivedMessage = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-                    Console.WriteLine("Received from client: {0}", receivedMessage);
+                    // Creation TCP/IP Socket using 
+                    // Socket Class Constructor
+                    Socket sender = new Socket(ipAddr.AddressFamily,
+                            SocketType.Stream, ProtocolType.Tcp);
 
-                    // Optionally, process the message here (e.g., log it)
+                    try
+                    {
 
-                    // Create a response (you can customize this as needed)
-                    string responseMessage = "Message received!";
-                    byte[] responseData = Encoding.ASCII.GetBytes(responseMessage);
+                        // Connect Socket to the remote 
+                        // endpoint using method Connect()
+                        sender.Connect(localEndPoint);
 
-                    // Send response back to the client
-                    stream.Write(responseData, 0, responseData.Length);
-                    Console.WriteLine("Sent back to client: {0}", responseMessage);
+                        // We print EndPoint information 
+                        // that we are connected
+                        Console.WriteLine("Socket connected to -> {0} ",
+                                    sender.RemoteEndPoint.ToString());
+
+                        // Creation of message that
+                        // we will send to Server
+                        byte[] messageSent = Encoding.ASCII.GetBytes("Test Client<EOF>");
+                        int byteSent = sender.Send(messageSent);
+
+                        // Data buffer
+                        byte[] messageReceived = new byte[1024];
+
+                        // We receive the message using 
+                        // the method Receive(). This 
+                        // method returns number of bytes
+                        // received, that we'll use to 
+                        // convert them to string
+                        int byteRecv = sender.Receive(messageReceived);
+                        Console.WriteLine("Message from Server -> {0}",
+                            Encoding.ASCII.GetString(messageReceived,
+                                                        0, byteRecv));
+
+                        // Close Socket using 
+                        // the method Close()
+                        sender.Shutdown(SocketShutdown.Both);
+                        sender.Close();
+                    }
+
+                    // Manage of Socket's Exceptions
+                    catch (ArgumentNullException ane)
+                    {
+
+                        Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                    }
+
+                    catch (SocketException se)
+                    {
+
+                        Console.WriteLine("SocketException : {0}", se.ToString());
+                    }
+
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                    }
+                }
+
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e.ToString());
                 }
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error: {0}", ex.Message);
-            }
-            finally
-            {
-                server.Stop();
-                Console.WriteLine("Server stopped.");
-            }
         }
-    }*/
+    }
+*/
     #endregion
 
     /// <summary>
